284b0e07b832e90c55d8c304b2c7e42351d27b7b
ArshBansal123 <76432668+ArshBansal123@users.noreply.github.com>
2021-12-17 17:55:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function setup() {
     canvas = createCanvas(640, 420);
     canvas.center;
     objectDetector= ml5.objectDetector('cocossd', modelLoaded);
-    documentgetElementById("status").innerHTML = "Status: Detecting Objects";
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
 }
 function modelLoaded() {
     console.log("Model Loaded!");
